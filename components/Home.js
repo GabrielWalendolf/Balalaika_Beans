0999849d2abe559cae5e5ac83665cd43a569f073
Gabriel Walendolf <gabriel.g.walendolf@gmail.com>
2024-11-29 16:48:31 -0300

    "Added comments to explain CSS styles in styles.css and reformatted HTML template in Home.js"

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,18 @@
 const Home = {
     template: `
-        <div class= "home-page">
-            <section class="text-center" style="padding: 80px 0">
-                <div class="text-container">
-                    <h1 style="color: #3d3bf3">Bem-vindo à Balalaika Beans</h1>
-                    <p>Onde tradição e magia se encontram em uma xícara perfeita.</p>
-                    <p>Desde 29 de dezembro de 1916, seguimos um legado único, inspirado pelas invenções misteriosas de Grigori Rasputin. Na Balalaika Beans, cada grão é escolhido com cuidado e paixão, seguindo tradições centenárias que combinam sabor, qualidade e um toque de magia.</p>
-                    <p>Prove um café que transcende o tempo e conecte-se à verdadeira essência do café russo, uma experiência que vai além do simples ato de degustar — é uma jornada interdimensional.</p>
-                </div>
-            </section>
-            <section class="container text-center my-5">
-                <b>Venha conhecer nossa história e descubra o que faz cada xícara da Balalaika Beans ser única.</b>
-            </section>
-        </div>
+    <div class= "home-page">
+        <section class="text-center" style="padding: 80px 0">
+            <div class="text-container">
+                <h1 style="color: #3d3bf3">Bem-vindo à Balalaika Beans</h1>
+                <p>Onde tradição e magia se encontram em uma xícara perfeita.</p>
+                <p>Desde 29 de dezembro de 1916, seguimos um legado único, inspirado pelas invenções misteriosas de Grigori Rasputin. Na Balalaika Beans, cada grão é escolhido com cuidado e paixão, seguindo tradições centenárias que combinam sabor, qualidade e um toque de magia.</p>
+                <p>Prove um café que transcende o tempo e conecte-se à verdadeira essência do café russo, uma experiência que vai além do simples ato de degustar — é uma jornada interdimensional.</p>
+            </div>
+        </section>
+        <section class="container text-center my-5">
+            <b>Venha conhecer nossa história e descubra o que faz cada xícara da Balalaika Beans ser única.</b>
+        </section>
+    </div>
     `,
 }
 export default Home;
